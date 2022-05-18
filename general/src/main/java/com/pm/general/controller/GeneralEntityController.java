1fa54c1b7b48ba1aa32be903f404123c594f0fd2
kimsiang1994 <kimsiangenquiries@gmail.com>
2022-05-18 22:01:21 +0800

    adding meeting minutes

---
+++ general/src/main/java/com/pm/general/controller/GeneralEntityController.java
@@ -0,0 +1,46 @@
+package com.pm.general.controller;
+
+import com.pm.general.dtos.GeneralDTO;
+import com.pm.general.service.GeneralService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/pm/general/")
+public class GeneralEntityController {
+
+    @Autowired
+    private GeneralService generalService;
+
+    @GetMapping(value = "{entityType}")
+    public GeneralDTO getEntityDetails(@PathVariable String entityType) {
+        log.info("GeneralEntityController -> getEntityDetails");
+        return generalService.getSpecificEntities(entityType);
+    }
+
+    @GetMapping(value = "{entityType}/{id}")
+    public GeneralDTO getSingleEntity(@PathVariable String entityType, @PathVariable Long id) {
+        return generalService.getSingleEntity(entityType, id);
+    }
+
+    @PostMapping(value = "{entityType}")
+    public void saveOrEditEntity(@PathVariable String entityType, @RequestBody Object entity) {
+        try {
+            generalService.saveOrEditEntity(entityType, entity);
+        } catch (HttpMessageNotReadableException exc) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Foo Not Found", exc);
+        }
+    }
+
+    @DeleteMapping(value = "{entityType}/{id}")
+    public void deleteEntity(@PathVariable String entityType, @PathVariable Long id) {
+        generalService.deleteEntity(entityType, id);
+    }
+
+}
